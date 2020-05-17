36cf14349d8d281f3cbf6d279c0d8eb792e28ad4
Paul Wilson <paulanthonywilson@gmail.com>
2020-05-17 13:11:56 +0100

    done

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -1,16 +1,45 @@
 defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
 
-  alias LiveViewStudio.Licences
+  alias LiveViewStudio.Licenses
+  import Number.Currency
+  import Inflex, only: [inflect: 2]
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 2, amount: Licences.calculate(2) )
+    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
     {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
-    Hello matey
+    <h1>Hello matey</h1>
+    <div id="license">
+      <div class="card">
+        <div class="content">
+          <div class="seats">
+            <img src="images/license.svg">
+            <span>
+              Your license is currently for
+              <strong><%=@seats %></strong> <%= inflect("seat", @seats) %>.
+             </span>
+          </div>
+          <form phx-change="update">
+            <input type="range" min="1" max="10" name="seats"
+              value="<%=@seats %>"/>
+          </form>
+          <div class="amount">
+            <%= number_to_currency(@amount) %>
+          </div
+        </div>
+      </div>
+    </div>
     """
+  end
+
+  def handle_event("update", %{"seats" => seats}, socket) do
+    seats = String.to_integer(seats)
+
+    socket = assign(socket, seats: seats, amount: Licenses.calculate(seats))
+    {:noreply, socket}
   end
 end
