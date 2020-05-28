8ffea7b2155b6d1164273e037c09c7413bee4c0c
Paul Wilson <paulanthonywilson@gmail.com>
2020-05-28 16:43:14 +0100

    autocomplete airports

--- lib/live_view_studio_web/live/flights_live.ex
+++ lib/live_view_studio_web/live/flights_live.ex
@@ -1,10 +1,18 @@
 defmodule LiveViewStudioWeb.FlightsLive do
   use LiveViewStudioWeb, :live_view
 
-  alias LiveViewStudio.Flights
+  alias LiveViewStudio.{Flights, Airports}
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, loading: false, flights: [], flight_code: "")
+    socket =
+      assign(socket,
+        loading: false,
+        flights: [],
+        flight_code: "",
+        airport_code: "",
+        airport_code_matches: []
+      )
+
     {:ok, socket}
   end
 
@@ -13,11 +21,23 @@ defmodule LiveViewStudioWeb.FlightsLive do
     <h1>Find a Flight</h1>
     <div id="search">
 
-      <form phx-submit="search-flights">
+      <form phx-submit="search-flight-code">
         <input type="text" name = "flight-code" placeholder = "flight-code"
           autofocus <%= if @loading, do: "readonly" %>  value="<%= @flight_code %>"/>
         <button type="submit"><img src="images/search.svg"/></button>
       </form>
+      <form phx-submit="search-airport" phx-change="change-airport">
+        <input type="text" maxlength="3" name="airport-code" placeholder="airport code"
+        value="<%=@airport_code %>" phx-debounce="1000" list="airport-matches" autocomplete="off"
+        <%= if @loading, do: "readonly" %>
+        />
+        <button type="submit"><img src="images/search.svg"/></button>
+      </form>
+      <datalist id="airport-matches">
+        <%= for airport <- @airport_code_matches  do %>
+          <option value="<%= airport %>"><%= airport %></airport>
+        <% end %>
+      </datalist>
 
       <%= if @loading do %>
         <div class="loader">...</div>
@@ -53,25 +73,45 @@ defmodule LiveViewStudioWeb.FlightsLive do
     """
   end
 
-  def handle_event("search-flights", %{"flight-code" => flight_code} , socket) do
-    send(self(), :do_search)
-    {:noreply, assign(socket, flight_code: flight_code, loading: true)}
+  def handle_event("search-flight-code", %{"flight-code" => flight_code}, socket) do
+    send(self(), :search_flight_code)
+    {:noreply, assign(socket, flight_code: flight_code, aiport_code: "", loading: true)}
+  end
+
+  def handle_event("search-airport", %{"airport-code" => airport_code}, socket) do
+    send(self(), :search_airport_code)
+    {:noreply, assign(socket, flight_code: "", airport_code: airport_code, loading: true)}
+  end
+
+  def handle_event("change-airport", %{"airport-code" => prefix}, socket) do
+    codes = Airports.suggest(prefix)
+    {:noreply, assign(socket, airport_code_matches: codes)}
   end
 
   defp format_time(time) do
     Timex.format!(time, "%d %b %Y at %H:%M", :strftime)
   end
 
-  def handle_info(:do_search, socket) do
+  def handle_info(:search_flight_code, socket) do
     %{flight_code: flight_code} = socket.assigns
     flights = Flights.search_by_number(flight_code)
-    socket = if flights == [] do
-      put_flash(socket, :info, "No flights for #{flight_code}")
-    else
-      clear_flash(socket)
-    end
-    {:noreply, assign(socket, loading: false, flights: flights)}
+    display_search_results(flights, flight_code, socket)
   end
 
+  def handle_info(:search_airport_code, socket) do
+    %{airport_code: airport_code} = socket.assigns
+    flights = Flights.search_by_airport(airport_code)
+    display_search_results(flights, airport_code, socket)
+  end
 
+  defp display_search_results(flights, code, socket) do
+    socket =
+      if flights == [] do
+        put_flash(socket, :info, "No flights for #{code}")
+      else
+        clear_flash(socket)
+      end
+
+    {:noreply, assign(socket, loading: false, flights: flights)}
+  end
 end
